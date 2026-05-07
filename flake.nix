ea73389c233a05482caf2842fa2a77b6be3fe596
zolszabo <kukkerem@gmail.com>
2026-05-07 17:20:46 +0200

    chore: bump flake version to v0.1.7

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
     let
       # Bumped by scripts/release.sh on each release tag.
       # Keep in sync with the git tag: v<version>.
-      version = "0.1.6";
+      version = "0.1.7";
 
       systems = [
         "x86_64-linux"
